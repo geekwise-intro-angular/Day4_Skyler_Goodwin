4cea1277592471f3521637e1da2b2507e0a68428
Blitzutu <Sky1460@comcast.net>
2016-04-14 19:41:57 -0700

    First Commit

---
+++ Day4.js
@@ -0,0 +1,9 @@
+angular.module('Day4', [])
+angular.module('Day4').controller('Ctrl' function(@scope){
+  $scope.message = "Welcome to the Site!";
+  scope.list = [
+    { title: "A", content:"Hey"},
+    { title: "B", content:"Howdy"},
+    { title: "C", content: "Hello"}
+  ]
+})
